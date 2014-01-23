f856c99ea09efdec91e6ddbf95c9bb42a797f538
Martin DeMello <martindemello@gmail.com>
2014-01-22 23:51:00 -0800

    read in extensions

--- acrosslite.rb
+++ acrosslite.rb
@@ -1,6 +1,7 @@
 require 'ostruct'
 
-HEADER_FORMAT = "v A12 v V2 A4 v2 A12 c c v v v"
+HEADER_FORMAT = "v A12 v V2 A4 v2 A12 c2 v3"
+EXT_HEADER_FORMAT = "A4 v2"
 
 FILE_MAGIC = 'ACROSS&DOWN'
 
@@ -17,15 +18,41 @@ def read_puz(filename)
   i = s.index(FILE_MAGIC)
   raise PuzzleFormatError unless i
 
-  header = s[(i - 2) .. (i - 2 + 0x34)]
-  out = header.unpack(HEADER_FORMAT)
+  # read the header
+  h_start, h_end = i - 2, i - 2 + 0x34
+  header = s[h_start .. h_end]
 
   c.global, _, c.cib, c.masked_low, c.masked_high,
     x.version, _, c.scrambled, _,
     x.width, x.height, x.n_clues, x.puzzletype, x.scrambled_state =
     header.unpack(HEADER_FORMAT)
 
+  # solution and fill = blocks of w*h bytes each
+  size = x.width * x.height
+  x.solution = s[h_end + 1, size]
+  x.fill = s[h_end + size + 1, size]
+  s = s[h_end + 2 * size + 1 .. -1]
+
+  # title, author, copyright, clues * n, notes = zero-terminated strings
+  x.title, x.author, x.copyright, *x.clues, x.notes, s =
+    s.split("\0", x.n_clues + 5)
+
+  # extensions: 8-byte header + len bytes data + \0
+  x.extensions = []
+  while (s.length > 8) do
+    e = OpenStruct.new
+    e.section, e.len, e.checksum = s.unpack(EXT_HEADER_FORMAT)
+    size = 8 + e.len + 1
+    break if s.length < size
+    e.data = s[8 ... size]
+    x.extensions << e
+    s = s[size .. -1]
+  end
+
   [c, x]
 end
 
-p read_puz ARGV[0]
+c, x = read_puz ARGV[0]
+p x.n_clues
+p x.clues.length
+p x.extensions
