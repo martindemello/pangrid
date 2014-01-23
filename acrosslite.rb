00a0a1fa578db25312c6bae7b79794b6b054d53f
Martin DeMello <martindemello@gmail.com>
2014-01-22 22:38:25 -0800

    read header

--- acrosslite.rb
+++ acrosslite.rb
@@ -9,14 +9,23 @@ end
 
 def read_puz(filename)
   s = IO.read(filename, :encoding => "ISO-8859-1")
-  xword = OpenStruct.new
+  # crossword
+  x = OpenStruct.new
+  # checksums
+  c = OpenStruct.new
 
   i = s.index(FILE_MAGIC)
   raise PuzzleFormatError unless i
 
   header = s[(i - 2) .. (i - 2 + 0x34)]
   out = header.unpack(HEADER_FORMAT)
-  p out
+
+  c.global, _, c.cib, c.masked_low, c.masked_high,
+    x.version, _, c.scrambled, _,
+    x.width, x.height, x.n_clues, x.puzzletype, x.scrambled_state =
+    header.unpack(HEADER_FORMAT)
+
+  [c, x]
 end
 
-read_puz ARGV[0]
+p read_puz ARGV[0]
