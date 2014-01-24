84bd5bba680caffda293593753a2576d9a6ed087
Martin DeMello <martindemello@gmail.com>
2014-01-23 22:34:40 -0800

    checksum reading and validation

--- acrosslite.rb
+++ acrosslite.rb
@@ -3,18 +3,49 @@ require 'ostruct'
 class PuzzleFormatError < Exception
 end
 
+# CRC checksum
+class Checksum
+  attr_accessor :sum
+
+  def self.of_string s
+    c = self.new(0)
+    c.add_string s
+    c.sum
+  end
+
+  def initialize(seed)
+    @sum = seed
+  end
+
+  def add_char(b)
+    low = sum & 0x0001
+    @sum = sum >> 1
+    @sum = sum | 0x8000 if low == 1
+    @sum = (sum + b) & 0xffff
+  end
+
+  def add_string(s)
+    s.bytes.map {|b| add_char b}
+  end
+
+  def add_string_0(s)
+    add_string (s + "\0") unless s.empty?
+  end
+end
+
 class AcrossLite
   # crossword, checksums
-  attr_accessor :x, :c
+  attr_accessor :xw, :cs
 
   HEADER_FORMAT = "v A12 v V2 A4 v2 A12 c2 v3"
+  HEADER_CHECKSUM_FORMAT = "c2 v3"
   EXT_HEADER_FORMAT = "A4 v2"
   EXTENSIONS = %w(LTIM GRBS RTBL GEXT)
   FILE_MAGIC = 'ACROSS&DOWN'
 
   def initialize
-    @x = OpenStruct.new
-    @c = OpenStruct.new
+    @xw = OpenStruct.new
+    @cs = OpenStruct.new
   end
 
   def read_puz(filename)
@@ -27,23 +58,23 @@ class AcrossLite
     h_start, h_end = i - 2, i - 2 + 0x34
     header = s[h_start .. h_end]
 
-    c.global, _, c.cib, c.masked_low, c.masked_high,
-      x.version, _, c.scrambled, _,
-      x.width, x.height, x.n_clues, x.puzzletype, x.scrambled_state =
+    cs.global, _, cs.cib, cs.masked_low, cs.masked_high,
+      xw.version, _, cs.scrambled, _,
+      xw.width, xw.height, xw.n_clues, xw.puzzle_type, xw.scrambled_state =
       header.unpack(HEADER_FORMAT)
 
     # solution and fill = blocks of w*h bytes each
-    size = x.width * x.height
-    x.solution = s[h_end + 1, size]
-    x.fill = s[h_end + size + 1, size]
-    s = s[h_end + 2 * size + 1 .. -1]
+    size = xw.width * xw.height
+    xw.solution = s[h_end, size]
+    xw.fill = s[h_end + size, size]
+    s = s[h_end + 2 * size .. -1]
 
     # title, author, copyright, clues * n, notes = zero-terminated strings
-    x.title, x.author, x.copyright, *x.clues, x.notes, s =
-      s.split("\0", x.n_clues + 5)
+    xw.title, xw.author, xw.copyright, *xw.clues, xw.notes, s =
+      s.split("\0", xw.n_clues + 5)
 
     # extensions: 8-byte header + len bytes data + \0
-    x.extensions = []
+    xw.extensions = []
     while (s.length > 8) do
       e = OpenStruct.new
       e.section, e.len, e.checksum = s.unpack(EXT_HEADER_FORMAT)
@@ -52,9 +83,12 @@ class AcrossLite
       break if s.length < size
       e.data = s[8 ... size]
       self.send(:"read_#{e.section.downcase}", e)
-      x.extensions << e
+      xw.extensions << e
       s = s[size .. -1]
     end
+
+    # verify checksums
+    raise PuzzleFormatError unless checksums == cs
   end
 
   # various extensions
@@ -84,15 +118,56 @@ class AcrossLite
       b == 0 ? b : b - 1
     }
   end
+
+  # checksums
+  def text_checksum(seed)
+    c = Checksum.new(seed)
+    c.add_string_0 xw.title
+    c.add_string_0 xw.author
+    c.add_string_0 xw.copyright
+    xw.clues.each {|cl| c.add_string cl}
+    c.add_string_0 xw.notes
+    c.sum
+  end
+
+  def header_checksum
+    h = [xw.width, xw.height, xw.n_clues, xw.puzzle_type, xw.scrambled_state]
+    Checksum.of_string h.pack(HEADER_CHECKSUM_FORMAT)
+  end
+
+  def global_checksum
+    c = Checksum.new header_checksum
+    c.add_string xw.solution
+    c.add_string xw.fill
+    text_checksum c.sum
+  end
+
+  def magic_checksums
+    mask = "ICHEATED".bytes
+    sums = [
+      text_checksum(0),
+      Checksum.of_string(xw.fill),
+      Checksum.of_string(xw.solution),
+      header_checksum
+    ]
+
+    l, h = 0, 0
+    sums.each_with_index do |sum, i|
+      l = (l << 8) | (mask[3 - i] ^ (sum & 0xff))
+      h = (h << 8) | (mask[7 - i] ^ (sum >> 8))
+    end
+    [l, h]
+  end
+
+  def checksums
+    c = OpenStruct.new
+    c.masked_low, c.masked_high = magic_checksums
+    c.cib = header_checksum
+    c.global = global_checksum
+    c.scrambled = 0
+    c
+  end
 end
 
 a = AcrossLite.new
 a.read_puz ARGV[0]
-p a.x.n_clues
-p a.x.clues.length
-#p x.extensions
-
-a.x.extensions.each do |e|
-  p e
-end
-
