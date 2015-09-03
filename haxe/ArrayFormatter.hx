d688ce2ed428ffffecf2fcccb887e13672062d8c
Martin DeMello <mdemello@google.com>
2015-09-03 00:49:17 -0700

    add an arrayformatter class

---
+++ haxe/ArrayFormatter.hx
@@ -0,0 +1,19 @@
+using StringTools;
+
+import Types;
+
+
+class ArrayFormatter {
+  public static function indent(xs: Strings, n: Int) : Strings {
+    var pad = ''.lpad(" ", n);
+    return [for (x in xs) pad + x];
+  }
+
+  public static function fence(xs: Strings, s: String) : String {
+    return s + xs.join(s) + s;
+  }
+
+  public static function toLines(xs: Strings) : String {
+    return xs.join("\n") + "\n";
+  }
+}
--- haxe/Text.hx
+++ haxe/Text.hx
@@ -1,4 +1,4 @@
-using StringTools;
+using ArrayFormatter;
 
 class Text implements Converter.XwWriter {
   public function new() { }
@@ -6,17 +6,16 @@ class Text implements Converter.XwWriter {
   public function write(xw : XWord) : String {
     var nums = xw.number();
     var rows = xw.map(function(f) { return f.toString(); });
-    var grid = [for (row in rows) row.join("")].join("\n");
-    var ac = format_row(nums.across, xw.clues.across, 2);
-    var dn = format_row(nums.down, xw.clues.down, 2);
-    return '$grid\n\nAcross:\n$ac\n\nDown:\n$dn\n';
+    var grid = [for (row in rows) row.join("")].toLines();
+    var ac = formatClues(nums.across, xw.clues.across, 2);
+    var dn = formatClues(nums.down, xw.clues.down, 2);
+    return '$grid\nAcross:\n$ac\nDown:\n$dn';
   }
 
-  function format_row(numbers : Array<Int>, clues: Array<String>, indent: Int) {
+  function formatClues(numbers : Array<Int>, clues: Array<String>, indent: Int) {
     // assert numbers.length == clues.length
     var n = numbers.length - 1;
-    var row = [for (i in 0 ... n)
-      "".lpad(" ", indent) + '${numbers[i]}. ${clues[i]}'];
-    return row.join("\n"); 
+    var row = [for (i in 0 ... n) '${numbers[i]}. ${clues[i]}'];
+    return row.indent(indent).toLines();
   }
 }
