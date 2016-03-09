3519caf49bd724e3feb2cc5f46c25b2711d8c822
Martin DeMello <mdemello@google.com>
2016-03-09 00:51:00 -0800

    sync logfile

--- lib/pangrid/frontend/webrick.rb
+++ lib/pangrid/frontend/webrick.rb
@@ -4,54 +4,54 @@ module Pangrid
 
 FORM = <<HERE
 <html>
-	<body>
+  <body>
     <form method="POST" enctype="multipart/form-data">
-			<input type="file" name="filedata" />
-			<select name="from">
-				<option value="across-lite-binary">AcrossLite binary (.puz)</option>
-				<option value="across-lite-text">AcrossLite text</option>
-			</select>
-			&rarr;
+      <input type="file" name="filedata" />
+      <select name="from">
+        <option value="across-lite-binary">AcrossLite binary (.puz)</option>
+        <option value="across-lite-text">AcrossLite text</option>
+      </select>
+      &rarr;
       <select name="to">
-				<option value="reddit-blank">Reddit (blank)</option>
-				<option value="reddit-filled">Reddit (filled)</option>
-				<option value="text">Text</option>
-			</select>
-			<input type="submit" />
+        <option value="reddit-blank">Reddit (blank)</option>
+        <option value="reddit-filled">Reddit (filled)</option>
+        <option value="text">Text</option>
+      </select>
+      <input type="submit" />
     </form>
     <hr>
     <div>
       <pre>%s</pre>
     </div>
-	</body>
+  </body>
 </html>
 HERE
 
 class Servlet < WEBrick::HTTPServlet::AbstractServlet
-	def do_GET (request, response)
-		response.status = 200
-		response.content_type = "text/html"
-		response.body = FORM % ""
-	end
+  def do_GET (request, response)
+    response.status = 200
+    response.content_type = "text/html"
+    response.body = FORM % ""
+  end
 
-	def do_POST(request, response)
-		input = request.query["filedata"]
-		from = Plugin.get(request.query["from"])
-		to = Plugin.get(request.query["to"])
-		reader = from.new
-		writer = to.new
+  def do_POST(request, response)
+    input = request.query["filedata"]
+    from = Plugin.get(request.query["from"])
+    to = Plugin.get(request.query["to"])
+    reader = from.new
+    writer = to.new
     out = nil
 
-		begin
-			out = writer.write(reader.read(input))
-		rescue Exception => e
-			out = e.inspect
-		end
+    begin
+      out = writer.write(reader.read(input))
+    rescue Exception => e
+      out = e.inspect
+    end
     
-		response.status = 200
-		response.content_type = "text/html"
-		response.body = FORM % WEBrick::HTMLUtils.escape(out)
-	end
+    response.status = 200
+    response.content_type = "text/html"
+    response.body = FORM % WEBrick::HTMLUtils.escape(out)
+  end
 end
 
 def self.run_webserver(port)
@@ -60,15 +60,16 @@ def self.run_webserver(port)
   puts "  http://localhost:#{port}"
   puts "-------------------------------------------"
 
-	Plugin.load_all
+  Plugin.load_all
 
-	log_stream = File.open('pangrid-webrick-access.log', 'w+')
-	log = [ [log_stream, WEBrick::AccessLog::COMMON_LOG_FORMAT] ]
+  logfile = File.open('pangrid-webrick-access.log', 'a')
+  logfile.sync = true
+  log = [ [logfile, WEBrick::AccessLog::COMMON_LOG_FORMAT] ]
 
-	server = WEBrick::HTTPServer.new(:Port => port, :AccessLog => log)
-	server.mount "/", Servlet
-	trap("INT") { server.shutdown }
-	server.start
+  server = WEBrick::HTTPServer.new(:Port => port, :AccessLog => log)
+  server.mount "/", Servlet
+  trap("INT") { server.shutdown }
+  server.start
 end
 
 end # module Pangrid
